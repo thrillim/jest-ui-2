8a2d7d52c5a4f758c46804baa92fd7699b5028ff
thrillim <86657389+thrillim@users.noreply.github.com>
2023-11-13 16:18:33 +0000

    run test successfully

---
+++ __test__/app/dashboard/page.test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import Dashboard from '../../../app/dashboard/page';
+
+describe('Dashboard', () => {
+  it('renders without crashing', () => {
+    // approach 1
+    const { getByTestId } = render(<Dashboard />);
+    expect(getByTestId('dashboard')).toBeInTheDocument();
+
+    // approach 2
+    // render(<Dashboard />);
+    // const dashboard = screen.getByTestId('dashboard');
+    // expect(dashboard).toHaveClass('blue');
+    // expect(dashboard).toHaveTextContent('Dashboard');
+  });
+});
---
+++ app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1 data-testid="dashboard" className="blue">Dashboard</h1>
+      <p>Welcome to your dashboard!</p>
+    </div>
+  );
+};
+
+export default Dashboard;
