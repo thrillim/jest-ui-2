f21b9a61d8be72c4d6d7b14945f29e0b70bf63dc
thrillim <86657389+thrillim@users.noreply.github.com>
2023-11-17 03:48:47 +0000

    test get started butotn

--- __test__/app/heroPage/page.test.jsx
+++ __test__/app/heroPage/page.test.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import HeroPage from "../../../app/heroPage/page";
 
-describe("HeroPage", () => {
+describe("HeroPage title and subtitle", () => {
   it("renders the hero title", () => {
     render(<HeroPage />);
     const heroTitle = screen.getByText(/Data to enrich your online business/i);
@@ -17,3 +17,15 @@ describe("HeroPage", () => {
     expect(heroSubtitle).toBeInTheDocument();
   });
 });
+
+describe("HeroPage Get started button", () => {
+  it("color of button", () => {
+    render(<HeroPage />);
+    const getStartedButton = screen.getByText(/Get Started/i);
+    fireEvent.mouseEnter(getStartedButton);
+    expect(getStartedButton).toHaveClass("hover:bg-indigo-500");
+    fireEvent.mouseLeave(getStartedButton);
+    expect(getStartedButton).toHaveClass("bg-indigo-600");
+  });
+})
+
